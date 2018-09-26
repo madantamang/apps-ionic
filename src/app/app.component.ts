438ab15f70c2ba185ed3192b9d8516379f79fa38
Madan Tamang <mtamang@resolutionmedia.com>
2018-09-26 17:01:30 +0545

    added some gallery pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { GalleryPage } from '../pages/gallery/gallery';
+
 
 @Component({
   templateUrl: 'app.html'
@@ -22,6 +24,7 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
+	  { title: 'Gallery', component: GalleryPage },
       { title: 'List', component: ListPage }
     ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { GalleryPage } from '../pages/gallery/gallery';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -13,6 +14,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
+	GalleryPage,
     ListPage
   ],
   imports: [
@@ -23,6 +25,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
+	GalleryPage,
     ListPage
   ],
   providers: [
